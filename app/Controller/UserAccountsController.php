0a5db445c6679f9f700cfebefbb344dd7f1f52e4
2rkmen <maotsedun@mail.ru>
2021-08-09 20:17:36 +0300

    05_02

--- app/Controller/UserAccountsController.php
+++ app/Controller/UserAccountsController.php
@@ -3,6 +3,8 @@ App::uses('AppController', 'Controller');
 
 class UserAccountsController extends AppController {
 	public function index() {
+		$salesForUser = 'Sales for a user';
+		$this->set('sales', $salesForUser);
 
 	}
 }
